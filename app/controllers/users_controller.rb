e83bed41130e2f52540c466ad630b14db9b9687f
Ranjith Jayaram <ranjith13041998@gmail.com>
2018-05-08 12:03:59 +0530

    finish user edit,update,index, destrot actions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,13 @@
 class UsersController < ApplicationController
-  before_action :logged_in_user, only: [:edit, :update]
+  before_action :logged_in_user, only: [:index, :edit, :update, :destroy]
   before_action :correct_user,   only: [:edit, :update]
-
+  before_action :admin_user,     only: :destroy
 
   def show
     @user = User.find(params[:id])
+  end
+  def index
+    @users = User.paginate(page: params[:page])
   end
 
   def new
@@ -33,6 +36,11 @@ class UsersController < ApplicationController
       render 'edit'
     end
   end
+  def destroy
+    User.find(params[:id]).destroy
+    flash[:success] = "User deleted"
+    redirect_to users_url
+  end
 
   private
 
@@ -52,5 +60,7 @@ class UsersController < ApplicationController
       redirect_to(root_url) unless current_user?(@user)
 
     end
-
+    def admin_user
+      redirect_to(root_url) unless current_user.admin?
+    end
 end
--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -5,6 +5,10 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     @user = users(:michael)
     @other_user = users(:archer)
   end
+  test "should redirect index when not logged in" do
+    get users_path
+    assert_redirected_to login_url
+  end
 
   test "should get new" do
     get signup_path
@@ -34,6 +38,20 @@ class UsersControllerTest < ActionDispatch::IntegrationTest
     patch user_path(@user), params: { user: { name: @user.name,
                                               email: @user.email } }
     assert flash.empty?
+    assert_redirected_to root_url
+  end
+  test "should redirect destroy when not logged in" do
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
+    assert_redirected_to login_url
+  end
+
+  test "should redirect destroy when logged in as a non-admin" do
+    log_in_as(@other_user)
+    assert_no_difference 'User.count' do
+      delete user_path(@user)
+    end
     assert_redirected_to root_url
   end
 end
