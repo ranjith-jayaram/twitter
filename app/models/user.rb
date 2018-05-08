2d5935fc25d7aa96d314aa03999e29fa89c95ab6
Ranjith Jayaram <ranjith13041998@gmail.com>
2018-05-08 19:29:51 +0530

    reset temp

--- app/models/user.rb
+++ app/models/user.rb
@@ -40,6 +40,10 @@ class User < ApplicationRecord
     update_attribute(:activated,    true)
     update_attribute(:activated_at, Time.zone.now)
   end
+    # Returns true if a password reset has expired.
+  def password_reset_expired?
+    reset_sent_at < 2.hours.ago
+  end
 
   # Sends activation email.
   def send_activation_email
