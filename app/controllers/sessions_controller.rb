9698ba0e9684f9e3b2e3622917265e270caaeac6
Ranjith Jayaram <ranjith13041998@gmail.com>
2018-05-10 15:20:28 +0530

    can get data from google in console

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,6 +4,7 @@ class SessionsController < ApplicationController
   end
 
   def create
+  	byebug
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
        if user.activated?
