99d1e76d481705c3fef0d9d2b508d2e8c4721f59
Ranjith Jayaram <ranjith13041998@gmail.com>
2018-05-14 12:26:42 +0530

    partial save

--- app/helpers/microposts_helper.rb
+++ app/helpers/microposts_helper.rb
@@ -1,7 +1,7 @@
 module MicropostsHelper
 	def display_micropost(micropost) 
 		content = h(micropost.content)
-		hash_tags = content.scan(/#(\w+)/)
+		hash_tags = content.scan(/\#(\w+)/)
 		hash_tags.flatten.uniq.each do |hash_tag|
 			hash_object = Hashtag.find_by(content: hash_tag)
 	    	hash_link = link_to(hash_tag, hash_object)
