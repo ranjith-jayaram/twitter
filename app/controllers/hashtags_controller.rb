48980e1a6a6ec5b53951a09da14d0a2c089f3a5b
Ranjith Jayaram <ranjith13041998@gmail.com>
2018-05-14 14:00:56 +0530

    save save save

--- app/controllers/hashtags_controller.rb
+++ app/controllers/hashtags_controller.rb
@@ -3,4 +3,11 @@ def show
 	@tag=Hashtag.find(params[:id])
 	@microposts=@tag.microposts.paginate(page: params[:page])
 end
+
+def followers
+    @title = "Followers"
+    @tag  = Hashtag.find(params[:id])
+    @users = @tag.followers.paginate(page: params[:page])
+end
+
 end
--- app/controllers/relationships_controller.rb
+++ app/controllers/relationships_controller.rb
@@ -2,54 +2,54 @@ class RelationshipsController < ApplicationController
   before_action :logged_in_user
 
   def create
+    @followed_type = params[:followed_type]
     if params[:followed_type]=="User"
-    @user = User.find_by(id: params[:followed_id])
-    current_user.follow(@user, "User")
-      respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
+      @user = User.find_by(id: params[:followed_id])
+      current_user.follow(@user, "User")
+        respond_to do |format|
+        format.html { redirect_to @user }
+        format.js
+      end
     elsif params[:followed_type]=="Hashtag"
-    @tag = Hashtag.find_by(id: params[:followed_id])
-    current_user.follow(@tag, "Hashtag")
-     respond_to do |format|
-      format.html { redirect_to @tag }
-      format.js
-    end 
+      @tag = Hashtag.find_by(id: params[:followed_id])
+      current_user.follow(@tag, "Hashtag")
+       respond_to do |format|
+        format.html { redirect_to @tag }
+        format.js
+      end 
     elsif params[:followed_type]=="Micropost"
-    @micropost = Micropost.find_by(id: params[:followed_id])
-    @hi=current_user.follow(@micropost, "Micropost")
-     respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end 
-  end
-
+      @micropost = Micropost.find_by(id: params[:followed_id])
+      @hi=current_user.follow(@micropost, "Micropost")
+       respond_to do |format|
+        format.html { redirect_to @user }
+        format.js
+      end 
+    end
   end
 
   def destroy
-  if params[:followed_type]=="User"
-    @user = Relationship.find(params[:id]).followed
-    current_user.unfollow_user(@user)
-      respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
+    @followed_type = params[:followed_type]
+    if params[:followed_type]=="User"
+      @user = Relationship.find(params[:id]).followed
+      current_user.unfollow_user(@user)
+        respond_to do |format|
+        format.html { redirect_to @user }
+        format.js
+      end
+    elsif params[:followed_type]=="Hashtag"
+      @tag = Relationship.find(params[:id]).followed
+      current_user.unfollow_hashtag(@tag)
+        respond_to do |format|
+        format.html { redirect_to @tag }
+        format.js
+      end
+    elsif params[:followed_type]=="Micropost"
+      @micropost = Relationship.find(params[:id]).followed
+      current_user.unfollow_micropost(@micropost)
+        respond_to do |format|
+        format.html { redirect_to @user }
+        format.js
+      end
     end
-  elsif params[:followed_type]=="Hashtag"
-    @tag = Relationship.find(params[:id]).followed
-    current_user.unfollow_hashtag(@tag)
-      respond_to do |format|
-      format.html { redirect_to @tag }
-      format.js
-    end
-  elsif params[:followed_type]=="Micropost"
-    @micropost = Relationship.find(params[:id]).followed
-    current_user.unfollow_micropost(@micropost)
-      respond_to do |format|
-      format.html { redirect_to @user }
-      format.js
-    end
-  end
-
   end
 end
